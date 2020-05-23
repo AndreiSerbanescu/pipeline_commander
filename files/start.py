9b4ee24f21696724c8f022dff601fbc2efdbebcb
Andrei Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2020-05-23 16:03:48 +0300

    use file cleaner to delete volumes older than six hours from directory

--- files/start.py
+++ files/start.py
@@ -6,6 +6,7 @@ from time import sleep
 from pipeline_handler import MultithreadedPipelineHandler
 import shutil
 import re
+from file_cleaner.file_cleaner import FileCleaner
 
 def start_streamlit_watcher(input_config_dir, result_config_dir):
 
@@ -27,6 +28,14 @@ def start_streamlit_watcher(input_config_dir, result_config_dir):
 
         sleep(0.5)
 
+def start_file_cleaner(clean_dir):
+
+    six_hours = 6 * 60 * 60
+    ten_minutes = 10 * 60
+
+    fs = FileCleaner(clean_dir, [".nii.gz", ".txt"], expiry_time_secs=six_hours, probe_interval=ten_minutes)
+    fs.start_watching()
+
 # PRE: config name of form config-idhere.json
 def __extract_config_id(config_name):
     return re.search('config-(.+?).json', config_name).group(1)
@@ -38,11 +47,15 @@ if __name__ == "__main__":
     config_dir = os.path.join(streamlit_share_path, "config")
     result_dir = os.path.join(streamlit_share_path, "result")
 
+    fs_base_dir = os.environ["FILESERVER_BASE_DIR"]
+
     os.makedirs(config_dir, exist_ok=True)
     os.makedirs(result_dir, exist_ok=True)
 
     streamlit_thread = Thread(target=start_streamlit_watcher, args=(config_dir, result_dir))
     fserver_thread = Thread(target=start_file_server)
+    file_cleaner_thread = Thread(target=start_file_cleaner, args=(fs_base_dir,))
 
     streamlit_thread.start()
     fserver_thread.start()
+    file_cleaner_thread.start()
