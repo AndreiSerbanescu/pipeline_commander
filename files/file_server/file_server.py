a9d9de5b678da54904fde694bf2f69a7c4f9f302
Andrei Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2020-05-27 23:29:09 +0300

    hide file server other files, do not display files in download dir

--- files/file_server/file_server.py
+++ files/file_server/file_server.py
@@ -3,12 +3,19 @@ import socketserver
 import os
 
 
+class HideListDirectoryHttpHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'hello.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
 class FileServer:
 
     def __init__(self, port, base_dir, handler=None):
         self.port = port
         self.handler = handler if handler is not None else http.server.SimpleHTTPRequestHandler
         self.base_dir = base_dir
+
     def start(self):
         os.chdir(self.base_dir)
         with socketserver.ThreadingTCPServer(("", self.port), self.handler) as httpd:
@@ -21,7 +28,12 @@ def start_file_server():
     fs_base_dir = os.environ["FILESERVER_BASE_DIR"]
     os.makedirs(fs_base_dir, exist_ok=True)
 
-    fs = FileServer(fs_port, fs_base_dir)
+    hello = "hello.html"
+    with open(os.path.join(fs_base_dir, hello), 'w') as hello_html:
+        hello_html.write("<!DOCTYPE html><html><head><!-- head definitions go here --></head>"
+                        "<body><!-- the content goes here --></body></html>")
+
+    fs = FileServer(fs_port, fs_base_dir, handler=HideListDirectoryHttpHandler)
     fs.start()
     
 if __name__ == "__main__":
